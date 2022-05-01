1ede51998463b81b03bbed21f5cdaadb8031a230
Kacper Bąk <kacper.bak@sonderfin.com>
2022-05-01 11:29:50 +0200

    feat: attach handling session to app

--- server.js
+++ server.js
@@ -16,6 +16,48 @@ const wss = new WebSocket.Server({
     noServer: true,
 });
 
+const sessionParser = session({
+    saveUninitialized: false,
+    secret: '$secret',
+    resave: false,
+});
+
+app.use(sessionParser);
+
+function checkSessions(request, response, next) {
+    console.log("checkSessions")
+    if (request.session.loggedin) {
+        next();
+    } else {
+        response.send({ loggedin: false });
+    }
+}
+
+function login(request, response) {
+    console.log("login")
+    if (!request.session.loggedin) {
+        request.session.loggedin = true;
+        request.session.user_name = request.params.user_name;
+        response.send({ loggedin: true });
+    }
+    response.send({ loggedin: true, user_name: request.session.user_name });
+}
+
+function logout(request, response) {
+    console.log("logout")
+    request.session.destroy();
+    response.send({ loggedin: false });
+}
+
+function loginTest(request, response) {
+    console.log("loginTest")
+    response.send({ loggedin: true, user_name: request.session.user_name });
+}
+
+app.get('/login/:user_name', [login]);
+app.get('/logout/', [checkSessions, logout]);
+app.get('/test/', [checkSessions, loginTest]);
+
 server.on('upgrade', function (request, socket, head) {
     wss.handleUpgrade(request, socket, head, function (ws) {
         wss.emit('connection', ws, request);
