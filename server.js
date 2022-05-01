a5af36917b1ef78db916543330cf04a1cbb5497f
Kacper Bąk <kacper.bak@sonderfin.com>
2022-05-01 09:31:19 +0200

    feat: creating endpoint, sending data in json format to client

--- server.js
+++ server.js
@@ -5,3 +5,18 @@ var PORT = process.env.PORT || 8080;
 var server = app.listen(PORT,() => console.log (`Listening on ${PORT}`));
 app.use(express.static(__dirname + '/static/'));
 app.use(cors());
+
+app.get ('/data/users',function(req,res) {
+    res.json({
+        users: [
+            {
+                name: 'John',
+                age: 25
+            },
+            {
+                name: 'Jane',
+                age: 30
+            },
+        ]
+    })
+})
