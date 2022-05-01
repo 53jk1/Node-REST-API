d343656fd5099676358f614c938a13f35629afa5
Kacper Bąk <kacper.bak@sonderfin.com>
2022-05-01 10:00:56 +0200

    feat: create an endpoint that returns a user with the given ID

--- server.js
+++ server.js
@@ -50,7 +50,20 @@ app.get("/user", (req, res, next) => {
     });
 });
 
-
+app.get("/user/:id", (req, res, next) => {
+    var sql = "select * from user where id = ?"
+    var params = [req.params.id]
+    db.get(sql, params, (err, row) => {
+        if (err) {
+            res.status(400).json({ error: err.message })
+            return
+        }
+        res.json({
+            message: 'success',
+            data: row
+        })
+    });
+});
 
 app.post('/data/users',function(req,res) {
     console.log(req.body)
