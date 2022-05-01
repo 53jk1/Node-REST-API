f247eded03d74fe044d02636d4a751f663b6b62b
Kacper Bąk <kacper.bak@sonderfin.com>
2022-05-01 09:57:22 +0200

    feat: connection to databases

--- server.js
+++ server.js
@@ -9,6 +9,32 @@ app.use(express.json());
 app.use(express.static(__dirname + '/static/'));
 app.use(cors());
 
+let db = new sqlite3.Database(DBSOURCE, (err) => {
+    if (err) {
+        console.error(err.message);
+    } else {
+        console.log('Connected to SQLite database.')
+        db.run(`CREATE TABLE user (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            name text,
+            age int,
+            password text
+        )`, (err) => {
+            if (err) {
+                console.error(err.message)
+            } else {
+                var insert = 'INSERT INTO user (name, age, password) VALUES (?, ?, ?)'
+                db.run(insert, ['John', 20, '123456'])
+                db.run(insert, ['Jane', 21, '123456789'])
+                db.run(insert, ['Jack', 22, 'Qwerty'])
+                db.run(insert, ['Jill', 23, 'Password'])
+                db.run(insert, ['Joe', 24, '12345'])
+
+            }
+        })
+    }
+})
+
 app.post('/data/users',function(req,res) {
     console.log(req.body)
     res.end("OK");
