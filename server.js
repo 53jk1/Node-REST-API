86fec89a48c69a5485e96b2140c0a3371846e219
Kacper Bąk <kacper.bak@sonderfin.com>
2022-05-01 09:43:57 +0200

    feat: to incorporate a module that facilitates the processing of data coming from HTTP (e.g., POST) requests.

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const cors = require('cors');
 var app = express();
 var PORT = process.env.PORT || 8080;
 var server = app.listen(PORT,() => console.log (`Listening on ${PORT}`));
+app.use(express.json());
 app.use(express.static(__dirname + '/static/'));
 app.use(cors());
 
