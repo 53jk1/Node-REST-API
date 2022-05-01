6ddc90a2f7313224d8f07d3adc9036e20cb63760
Kacper Bąk <kacper.bak@sonderfin.com>
2022-05-01 09:52:40 +0200

    feat: declare database file name

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var sqlite3 = require ('sqlite3').verbose()
+const DBSOURCE = "db.sqlite"
 const cors = require('cors');
 var app = express();
 var PORT = process.env.PORT || 8080;
