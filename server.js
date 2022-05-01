8f87ea074392e2200c823066b34ba436febc5c6c
Kacper Bąk <kacper.bak@sonderfin.com>
2022-05-01 09:27:39 +0200

    fix: inclusion of a module to fix a problem with CORS security

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 var express = require('express');
+const cors = require('cors');
 var app = express();
 var PORT = process.env.PORT || 8080;
 var server = app.listen(PORT,() => console.log (`Listening on ${PORT}`));
