9ffb5123c4b661e9c0b89b1609ade85684f2378b
Kacper Bąk <kacper.bak@sonderfin.com>
2022-05-01 09:23:43 +0200

    fix: static dirname

--- server.js
+++ server.js
@@ -2,4 +2,4 @@ var express = require('express');
 var app = express();
 var PORT = process.env.PORT || 8080;
 var server = app.listen(PORT,() => console.log (`Listening on ${PORT}`));
-app.use(express.static(__dirname + '/assets/'));
+app.use(express.static(__dirname + '/static/'));
